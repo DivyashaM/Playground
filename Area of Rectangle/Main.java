283ce34715f9aedc6af4a66946f0681b31fa5400
DivyashaM <43409038+DivyashaM@users.noreply.github.com>
2019-01-16 11:06:25 +0530

    Test code pushed to repository.

---
+++ Area of Rectangle/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+       int len=sc.nextInt();
+       int bre=sc.nextInt();
+       int ar=len*bre;
+      System.out.println(ar);
+	}
+}
